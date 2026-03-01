e37558121133ba49a2077ad156f60abc48d8b928
eungbin <programmer97@naver.com>
2026-03-01 22:39:47 +0900

    feat(style): 전역 스타일 추가 (#12) - primary(#0352ff) 색상 적용 (ex. text-primary, bg-primary) - secondary(100, 200, 300, 400) 색상 적용 (ex. text-secondary-100, bg-secondary-400)

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import App from "../App";
+import Community from "../pages/community/Community"
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/community" element={<Community />} />
     </Routes>
   );
 }
