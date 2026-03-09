0ec49f79ed95fd48cfaa0ef9cb7380b15706a14b
eungbin <programmer97@naver.com>
2026-03-09 23:54:15 +0900

    feat(레이아웃-헤더): 뒤로가기 버튼 추가 (#17) - back-arrow.svg 추가 - 경로의 depth가 2이상인 경우 뒤로가기 버튼 표시 - 현재 기준의 예시 - '/': 0, '/community':1, '/community/6': 2 로 정의

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -9,10 +9,14 @@ export default function MobileLayout() {
   const hasHeader: boolean = isHome || isCommunity
   const headerTitle: string = isCommunity ? "테마 커뮤니티" : "고정 헤더"
 
+  // 뒤로가기 버튼을 보여주어야 하는 경로인지 판별
+  // 현재 기준은 depth가 2이상인 경우 ('/'의 경우 0으로 보고, '/community'의 경우 1로 보고, '/community/6'의 경우 2로 봄)
+  const showBackArrow: boolean = pathname.split('/').length >= 3;
+
   return (
     <div className="flex min-h-screen items-start justify-center bg-white py-4">
       <div className="relative flex h-[700px] w-[340px] flex-col overflow-hidden border border-secondary-200">
-        {hasHeader && <MobileHeader title={headerTitle} />}
+        {hasHeader && <MobileHeader title={headerTitle} showBackArrow={showBackArrow} />}
 
         <div className="scrollbar-hidden flex-1 overflow-y-auto pb-16">
           <Outlet />
