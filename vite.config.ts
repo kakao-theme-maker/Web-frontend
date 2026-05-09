05c1a08d095521c0e9c52eba1987b4019f3047b3
6108 <6108@users.noreply.github.com>
2026-05-09 17:15:50 +0900

    chore: setup @ alias for src
    - 절대 경로 별칭 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
     react(),
     svgr()
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   // server: {
   //   proxy: {
   //     '/api': {
